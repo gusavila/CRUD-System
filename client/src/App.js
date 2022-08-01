3e754fb51b9cd554e9b7929ed798b6ca415f9531
Gustavo Avila <gustavohenriqueavila10@gmail.com>
2022-08-01 16:51:49 -0300

    crud added to the repository

---
+++ client/src/App.js
@@ -0,0 +1,115 @@
+import React, {useState, useEffect } from "react"
+import "./App.css";
+import Axios from "axios";
+import Card from "./components/cards/card";
+import "./components/cards/card.css";
+function App() {
+  const [values, setValues] = useState();
+  const [listPessoa, setListPessoa] = useState();
+  console.log(listPessoa)
+  const handleChangeValues = (value) => {
+    setValues(prevValue=>({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }))
+  };
+
+  const handleClickButton = () =>{
+    Axios.post("http://localhost:3001/register",{
+      name: values.name,
+      cpf: values.cpf,
+      birth: values.birthDate,
+      registerDate: values.registerDate,
+      ativo: values.userVisits,
+    }).then((response)=>{
+      console.log(response);
+    });
+  };
+
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/getCards").then((response) => {
+      setListPessoa(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="app--container container">
+      <div className="register--container">
+        <h1 className="register--title">Cadastro</h1>
+
+          <input 
+          type="text" 
+          name="name" 
+          placeholder="Nome" 
+          className="register--input"
+          onChange={handleChangeValues}
+          />
+
+          <input 
+          type="text" 
+          name="cpf" 
+          placeholder="CPF" 
+          className="register--input"
+          onChange={handleChangeValues}
+          />
+
+          <input 
+          type="date" 
+          name="birthDate" 
+          placeholder="Data de Nascimento" 
+          className="register--input"
+          onChange={handleChangeValues}
+          />
+
+          <input 
+          type="date" 
+          name="registerDate" 
+          placeholder="Data de Cadastro" 
+          className="register--input"
+          onChange={handleChangeValues}
+          />
+          
+          <div className="radio--input">
+            <p className="question">Usuário Ativo?</p>
+            <input
+            type="radio"
+            id="active"
+            name="userVisits"
+            value="S"
+            onChange={handleChangeValues}
+            />
+            <label htmlFor="active">Sim</label>
+            
+            <input
+            type="radio"
+            id="no-active"
+            name="userVisits"
+            value="N"
+            onChange={handleChangeValues}
+            />
+            <label htmlFor="no-active">Não</label>
+          </div>
+
+          <button className="register--button" 
+          onClick={() => handleClickButton()}>Cadastrar</button>
+      </div>
+      {typeof listPessoa !== "undefined" && listPessoa.map((value) => {
+        return (
+        <Card
+         key={value.id} 
+         listCard={listPessoa} 
+         setListCard={setListPessoa}
+         id={value.IDPessoa}
+         name= {value.Nome}
+         cpf=  {value.CPF}
+         birth= {value.DataNascimento}
+         registerDate= {value.DataCadastro}
+         ativo= {value.Ativo}
+         ></Card>
+         );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/cards/dialog/dialog.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Axios from "axios";
+
+export default function FormDialog(props) {
+  const [editValues, setEditValues] = useState({
+    id: props.id,
+    name: props.name,
+    cpf: props.cpf,
+    birth: props.birth,
+    registerDate: props.registerDate,
+  });
+
+  const handleClickOpen = () => {
+    props.setOpen(true);
+  };
+
+  const handleClose = () => {
+    props.setOpen(false);
+  };
+
+  const handleChangeValues = (value) => {
+    setEditValues((prevValues) => ({
+      ...prevValues,
+      [value.target.id]: value.target.value,
+    }));
+  };
+
+  const handleEditPessoa = () => {
+    Axios.put("http://localhost:3001/edit", {
+      id: editValues.id,
+      name: editValues.name,
+      cpf: editValues.cpf,
+      birth: editValues.birth,
+      registerDate: editValues.registerDate
+    })
+    handleClose();
+  };
+
+  const handleDeletePessoa = () => {
+    Axios.delete(`http://localhost:3001/delete/${editValues.id}`); 
+  };
+
+  const handleDeleteGame = () => {
+    Axios.delete(`http://localhost:3001/delete/${editValues.id}`).then(() => {
+      props.setListCard(
+        props.listCard.filter((value) => {
+          return value.id !== editValues.id;
+        })
+      );
+    });
+    handleClose();
+  };
+
+  return (
+      <Dialog
+        open={props.open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Editar</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Nome"
+            defaultValue={props.name}
+            type="text"
+            onChange={handleChangeValues}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="cpf"
+            label="CPF"
+            defaultValue={props.cpf}
+            type="text"
+            onChange={handleChangeValues}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="birth"
+            defaultValue={props.birth}
+            type="date"
+            onChange={handleChangeValues}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="registerDate"
+            defaultValue={props.registerDate}
+            type="date"
+            onChange={handleChangeValues}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button color="primary" onClick={() => handleDeleteGame()}>
+            Excluir
+          </Button>
+          <Button color="primary" onClick={() => handleEditPessoa()}>
+            Salvar
+          </Button>
+        </DialogActions>
+      </Dialog>
+  );
+}
